11a781623505c660e05e9a3ad19353a86869125e
deebuls <deebuls@gmail.com>
2015-06-29 11:42:45 +0200

    #4 Updated Documentation

--- mcr_manipulation/mcr_torque_comparator/ros/include/torque_comparator/torque_comparator.h
+++ mcr_manipulation/mcr_torque_comparator/ros/include/torque_comparator/torque_comparator.h
@@ -34,42 +34,125 @@
 #include <Eigen/Dense>
 
 namespace mcr_torque_comparator {
+/**
+ * Torque comparator compares the difference between the measured torque and
+ * the calculated torque.
+ * Measured Torque is provided by the subscribing to /joint_states
+ * Calculated Torque is provided by the /torque_calculated
+ *
+ */
 class TorqueComparator: public nodelet::Nodelet
 {
 public:
+
+    /**
+     * Ros Node handle pointer
+     */
     ros::NodeHandle *nh_;
+
+    /**
+     * ROS Subscriber for Joint States
+     */
     message_filters::Subscriber<sensor_msgs::JointState> sub_joint_states_;
+
+    /**
+     * ROS Subcriber for Calculated Torques
+     */
     message_filters::Subscriber<sensor_msgs::JointState> sub_calculated_torques_;
+
     /*
-     * Synchronized subscriber
+     * Synchronized subscriber for bot Calculated Torque and measured Torque
      */
     message_filters::TimeSynchronizer<sensor_msgs::JointState,sensor_msgs::JointState> *sync_;
-    // force estimation from joint efforts
+
+    /**
+     * Difference Torque publisher
+     */
 	ros::Publisher cmd_torque_publisher_;
 
+
+    /**
+     * Constructor
+     */
     TorqueComparator();
+
+    /**
+     * Destructor
+     */
     virtual ~TorqueComparator();
 
+    /**
+     * Synchronize call back
+     * @param joints  torque measured
+     * @param calculate_joints torques calculated
+     */
     void syncCallback(const sensor_msgs::JointStateConstPtr &joints,
                             const sensor_msgs::JointStateConstPtr &calculate_joints ) ;
-    
 private:
 
+    /**
+     * Topic name for Joint states
+     */
 	std::string joint_state_topic_ ;
+
+
+    /**
+     * tool tip name 
+     */
 	std::string tooltip_name_;
+
+    /**
+     * root name for the arm
+     */
 	std::string root_name_;
+
+    /**
+     * Degree of Freedom
+     */
     unsigned int DOF_;
-    
+
+    /**
+     * KDL chain 
+     */
     KDL::Chain arm_chain_;
+
+    /**
+     * Joint limits of the arm
+     */
     std::vector<boost::shared_ptr<urdf::JointLimits> > joint_limits_;
+
+    /**
+     * Joint positions
+     */
     KDL::JntArray joint_positions_;
+
+    /**
+     * joint torques
+     */
     KDL::JntArray joint_torques_;
 
+
+    /**
+     * calculated torques
+     */
     brics_actuator::JointTorques joint_brics_msg_ ;
 
+
+    /*
+     * Nodelet init function 
+     * Loads the URDF file 
+     * Initilaized the KDL chain 
+     * resizes all the vectors in the class
+     * ROS subscripton and publisher
+     * 
+     */
     virtual void onInit();
+
+    /**
+     * Initializes the joint messages 
+     */
     void initJointMsgs() ;
-    bool sendEstimatedWrench();
+
 
 };
 
